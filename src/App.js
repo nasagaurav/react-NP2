453e5fbddfd82768976007d7b8f8078db2ca7082
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-13 16:06:24 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to c happen :)</p>
+      <p>Start editing to c happening :)</p>
     </div>
   );
 }
